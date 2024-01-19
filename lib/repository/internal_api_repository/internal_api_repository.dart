129f9bf18f072afea60353a71ecf0da2c9827a74
Biel Carpi <biel.carpi@outlook.com>
2024-01-19 16:22:16 +0100

    feat(api): websocket improvements

--- lib/repository/internal_api_repository/internal_api_repository.dart
+++ lib/repository/internal_api_repository/internal_api_repository.dart
@@ -4,13 +4,14 @@ import 'dart:typed_data';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
 import 'package:nia_flutter/utils/logs/logs.dart';
 import 'package:web_socket_channel/io.dart';
 
 class InternalAPIRepository extends GetxController {
   static InternalAPIRepository get instance => Get.find();
 
-  final API_URL = "https://nia-backend.oa.r.appspot.com/api";
+  final API_URL = "wss://nia-backend.oa.r.appspot.com/api";
   final SEND_AUDIO_ENDPOINT = "/audio";
   final TEXT_TO_SPEECH_ENDPOINT = "/tts";
 
@@ -21,8 +22,8 @@ class InternalAPIRepository extends GetxController {
   bool isRecording = false;
 
   // Initialize WebSocket connection
-  void initWebSocket(String url) {
-    channel = IOWebSocketChannel.connect(Uri.parse(url));
+  void initWebSocket() {
+    channel = IOWebSocketChannel.connect(Uri.parse(API_URL + SEND_AUDIO_ENDPOINT));
     channel!.stream.listen(
           (data) {
         if (data is String) {
@@ -30,7 +31,7 @@ class InternalAPIRepository extends GetxController {
           processTextData(data);
         } else if (data is Uint8List) {
           // Handle binary audio data
-          playAudioStream(data);
+          _playAudioStream(data);
         }
       },
       onDone: () {
@@ -44,13 +45,8 @@ class InternalAPIRepository extends GetxController {
 
   // Start recording and sending audio data
   void startRecording() async {
-    audioRecorder = FlutterSoundRecorder();
+    audioRecorder = FlutterSoundRecorder(logLevel: Level.error);
     await audioRecorder!.openRecorder();
-
-    audioRecorder!.startRecorder(
-      toStream: streamController.sink,
-    );
-    isRecording = true;
 
     // Listen to the stream and send data over WebSocket
     streamController.stream.listen((data) {
@@ -58,24 +54,35 @@ class InternalAPIRepository extends GetxController {
         channel!.sink.add(data);
       }
     });
+
+    audioRecorder!.startRecorder(
+      codec: Codec.pcm16,
+      numChannels: 1,
+      sampleRate: 44100,
+      toStream: streamController.sink,
+    );
+    isRecording = true;
   }
   // Stop recording
   void stopRecording() async {
     if (isRecording) {
       await audioRecorder!.stopRecorder();
       await audioRecorder!.closeRecorder();
+      streamController.close(); // Close the stream controller
+
+      channel!.sink.add("END_OF_AUDIO");
+
       audioRecorder = null;
       isRecording = false;
     }
-  }
+  }  // Process received text data
 
-  // Process received text data
   void processTextData(String text) {
     Logs.d("Received text: $text");
   }
 
   // Play audio data as it's received
-  void playAudioStream(Uint8List audioData) async {
+  void _playAudioStream(Uint8List audioData) async {
     if (audioPlayer == null) {
       audioPlayer = FlutterSoundPlayer();
       await audioPlayer!.openPlayer();
